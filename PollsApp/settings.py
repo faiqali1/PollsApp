54f64e5a1b5c404b213737cb75bd43ac7aa1722f
Syed Faiq Ali Faisal <faiqali2010@hotmail.com>
2021-12-03 16:14:13 +0500

    Completed 3.5

--- PollsApp/settings.py
+++ PollsApp/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps:
+
     'polls.apps.PollsConfig',
+
+    # Third-party Apps:
 ]
 
 MIDDLEWARE = [
