c0146f60e536a32f015194f92e5c66291f7bba8f
Syed Faiq Ali Faisal <faiqali2010@hotmail.com>
2021-12-04 19:38:45 +0500

    Working bootstrap integration

--- polls/views.py
+++ polls/views.py
@@ -8,7 +8,8 @@ from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
+    template_name = 'polls/All-polls.html'
+    # template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
